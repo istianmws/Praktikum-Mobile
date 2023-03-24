71e1f86fdd42e420c7501ffed2a9633e01a3b722
istianmws <istianmuhammad@gmail.com>
2023-03-24 10:28:10 +0700

    Quiz TPM Grocery APP

---
+++ lib/login.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import 'list_belanja.dart';
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text("Login Page")
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Icon(Icons.storefront_outlined, color: Colors.teal,),
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.person),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      fillColor: Colors.grey.shade200,
+                      filled: true,
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.grey[700]),
+                      hintText: "Masukkan Email",
+                      hintStyle: TextStyle(color: Colors.grey[500])
+                  ),
+                  onSaved: (String? value) {
+                  },
+                  validator: (String? value) {
+                    return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                  },
+
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.lock),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      fillColor: Colors.grey.shade200,
+                      filled: true,
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.grey[700]),
+                      hintText: "Masukkan Password",
+                      hintStyle: TextStyle(color: Colors.grey[500])
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ElevatedButton(
+                  onPressed: (){},
+                  // decoration: BoxDecoration(
+                  //   color: Colors.black,
+                  //   borderRadius: BorderRadius.circular(8),
+                  // ),
+                  child: Text('Login'),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context){
+                      return ListPage();
+                    }));
+                  },
+                  child: Text('Lupa Password?'))
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:praktikum_mobile/login_page.dart';
-import 'package:praktikum_mobile/login_pagee.dart';
-import 'package:praktikum_mobile/quiz%20teori/container_page.dart';
-import 'package:praktikum_mobile/quiz%20teori/segitasiku_page.dart';
+
+import 'list_belanja.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login',
+      title: 'Grocery App',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
 
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: ListPage(),
     );
   }
 }
