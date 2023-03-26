7d60010956cefb65defd2cdae35a3f6e90c4d185
istianmws <istianmuhammad@gmail.com>
2023-03-26 15:52:37 +0700

    Revert "Adjusment Directory"
    This reverts commit dc1015e6

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:praktikum_mobile/quiz%20teori/container_page.dart';
-import 'package:praktikum_mobile/quiz%20teori/segitasiku_page.dart';
+import 'package:praktikum_mobile/login.dart';
+
+import 'list_belanja.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login',
+      title: 'Grocery App',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
 
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPageGroceryApp(),
     );
   }
 }
